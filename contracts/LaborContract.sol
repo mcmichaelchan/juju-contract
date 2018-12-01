5986f0b6a4574bfd37d06320131e796298d794d8
mcmicahelchan <623769018@qq.com>
2018-12-01 23:57:00 +0800

    feat(Contract): change some detail of the contract

--- contracts/LaborContract.sol
+++ contracts/LaborContract.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.17;
 
 contract LaborContractList {
     address[] laborContracts;
-    function createLaborContract(string _partyA_name, uint _startDate, uint 
-    _endDate, uint _salary, string _job) public {
-        address newLaborContract = new LaborContract(_partyA_name, _startDate, _endDate, _salary, _job, msg.sender);
+    function createLaborContract(uint _startDate, uint 
+    _endDate, uint _salary, string _job, address _partyB) public {
+        address newLaborContract = new LaborContract(_startDate, _endDate, _salary, _job, msg.sender, _partyB);
         laborContracts.push(newLaborContract);
     }
     function getContracts() public view returns(address[]) {
@@ -27,8 +27,6 @@ contract LaborContract {
     }
     address public partyA;
     address public partyB;
-    string public partyA_name;
-    string public partyB_name;
     uint public startDate;
     uint public endDate;
     uint public salary;
@@ -36,10 +34,10 @@ contract LaborContract {
     Modification[] public modifyHistory;
     Sign[] public signHistory;
     
-    constructor(string _partyA_name, uint _startDate, uint 
-    _endDate, uint _salary, string _job, address _partyA) public {
+    constructor(uint _startDate, uint 
+    _endDate, uint _salary, string _job, address _partyA, address _partyB) public {
         partyA = _partyA;
-        partyA_name = _partyA_name;
+        partyB = _partyB;
         startDate = _startDate;
         endDate = _endDate;
         salary = _salary;
@@ -66,14 +64,14 @@ contract LaborContract {
         }
     }
 
-    function getSummary() public view returns (string, uint, uint, uint, string, address) {
+    function getSummary() public view returns (uint, uint, uint, string, address, address) {
         return (
-            partyA_name,
             startDate,
             endDate,
             salary,
             job,
-            partyA
+            partyA,
+            partyB
         );
     }    
 }
