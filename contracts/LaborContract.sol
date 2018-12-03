29dccc051b02ad19bea532856337fde1b246fca8
mcmichaelchan <623769018@qq.com>
2018-12-04 00:49:20 +0800

    feat(LaborContract): add detail cgi

--- contracts/LaborContract.sol
+++ contracts/LaborContract.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.17;
 contract LaborContractList {
     address[] laborContracts;
     function createLaborContract(uint _startDate, uint 
-    _endDate, uint _salary, string _job, address _partyB) public {
-        address newLaborContract = new LaborContract(_startDate, _endDate, _salary, _job, msg.sender, _partyB);
+    _endDate, uint _salary, string _job, address _partyB, string name) public {
+        address newLaborContract = new LaborContract(_startDate, _endDate, _salary, _job, msg.sender, _partyB, name);
         laborContracts.push(newLaborContract);
     }
     function getContracts() public view returns(address[]) {
@@ -31,17 +31,28 @@ contract LaborContract {
     uint public endDate;
     uint public salary;
     string public job;
+    string public name;
     Modification[] public modifyHistory;
     Sign[] public signHistory;
     
     constructor(uint _startDate, uint 
-    _endDate, uint _salary, string _job, address _partyA, address _partyB) public {
+    _endDate, uint _salary, string _job, address _partyA, address _partyB, string _name) public {
         partyA = _partyA;
         partyB = _partyB;
         startDate = _startDate;
         endDate = _endDate;
         salary = _salary;
         job = _job;
+        name = _name;
+    }
+    
+    function getSummary() public view returns (string, address, address, string) {
+        return (
+            job,
+            partyA,
+            partyB,
+            name
+        );
     }
     
     function sign() public{
@@ -63,15 +74,17 @@ contract LaborContract {
             partyB = msg.sender;
         }
     }
+    
 
-    function getSummary() public view returns (uint, uint, uint, string, address, address) {
+    function getDetail() public view returns (uint, uint, uint, string, address, address, string) {
         return (
             startDate,
             endDate,
             salary,
             job,
             partyA,
-            partyB
+            partyB,
+            name
         );
     }    
 }
